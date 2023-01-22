4dba59a04c12fde83763671f5d65ba15df64f510
skyman529 <skyman529@gmail.com>
2023-01-22 15:13:01 -0700

    updates

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -47,12 +47,12 @@ export const getDb = async () => {
   const store = tx.objectStore("jate");
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
   console.log("result.value", result);
-  return result;
+  return result.value;
 };
 
 initdb();
